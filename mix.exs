5656ee952096ab443d020a1d7c39415b82a68de8
Michael Crumm <michael.crumm@cargosense.com>
2022-11-01 11:34:24 -0700

    Add phx_live_storybook

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule BulmaComponents.MixProject do
     [
       app: :bulma_components,
       version: "0.1.0",
-      elixir: "~> 1.14",
+      elixir: "~> 1.13",
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
@@ -21,8 +21,11 @@ defmodule BulmaComponents.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      {:phoenix_live_view, "~> 0.18.0"},
+
+      # Dev/test dependencies
+      {:phx_live_storybook, "~> 0.4.0", only: :dev},
+      {:jason, "~> 1.0", optional: true}
     ]
   end
 end
