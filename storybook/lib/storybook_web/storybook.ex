1a510193a3926491fa7fb897b0d356326972c842
David Parry <david.parry@suranyami.com>
2024-02-19 15:57:16 +1100

    Add my fontawesome free kit details
    Not sure what a better way to do this is.

--- storybook/lib/storybook_web/storybook.ex
+++ storybook/lib/storybook_web/storybook.ex
@@ -3,7 +3,10 @@ defmodule StorybookWeb.Storybook do
     otp_app: :storybook_web,
     content_path: Path.expand("../../storybook", __DIR__),
     # assets path are remote path, not local file-system paths
-    css_path: "/assets/app.css",
+    css_path: "/assets/storybook.css",
     js_path: "/assets/storybook.js",
-    sandbox_class: "storybook-web"
+    sandbox_class: "storybook-web",
+    font_awesome_plan: :free,
+    font_awesome_kit_id: "a86d7ff2ff",
+    font_awesome_rendering: :webfont
 end
