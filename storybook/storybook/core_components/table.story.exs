d40eb47f2d5c320d8cf736ba700cd5543b2c13ac
David Parry <david.parry@suranyami.com>
2024-02-19 15:58:34 +1100

    Somewhat improved table story

--- storybook/storybook/core_components/table.story.exs
+++ storybook/storybook/core_components/table.story.exs
@@ -1,41 +1,87 @@
 defmodule Storybook.CoreComponents.Table do
   use PhoenixStorybook.Story, :component
-  alias StorybookWeb.CoreComponents
 
-  def function, do: &CoreComponents.table/1
-  def aliases, do: [Storybook.CoreComponents.Table.User]
+  def function, do: &StorybookWeb.Components.Table.table/1
+
+  @rows [
+    %{first_name: "Jean", last_name: "Dupont", city: "Paris"},
+    %{first_name: "Sam", last_name: "Smith", city: "NY", selected: true},
+    %{first_name: "John", last_name: "Doe", city: "London"},
+    %{first_name: "Jane", last_name: "Doe", city: "Rome"}
+  ]
+  @slots [
+    """
+    <:col :let={user} label="First name">
+      <%= user.first_name %>
+    </:col>
+    """,
+    """
+    <:col :let={user} label="Last name">
+      <%= user.last_name %>
+    </:col>
+    """,
+    """
+    <:col :let={user} label="City">
+      <%= user.city %>
+    </:col>
+    """
+  ]
 
   def variations do
+    List.flatten([
+      sample(),
+      modifiers(),
+      all_mods()
+    ])
+  end
+
+  def sample() do
     [
       %Variation{
-        id: :table,
-        attributes: %{
-          rows:
-            {:eval,
-             ~s"""
-             [
-               %User{id: 1, username: "jose"},
-               %User{id: 2, username: "chris"}
-             ]
-             """}
-        },
-        slots: [
-          """
-          <:col :let={user} label="Id">
-            <%= user.id %>
-          </:col>
-          """,
-          """
-          <:col :let={user} label="User name">
-            <%= user.username %>
-          </:col>
-          """
-        ]
+        id: :default,
+        description: "Default",
+        attributes: %{rows: @rows},
+        slots: @slots
       }
     ]
   end
-end
 
-defmodule Storybook.CoreComponents.Table.User do
-  defstruct [:id, :username]
+  @modifiers ~w[bordered striped narrow hoverable fullwidth]a
+
+  def modifiers do
+    Enum.map(@modifiers, fn modifier ->
+      %Variation{
+        id: :"table_#{modifier}",
+        description: Phoenix.Naming.humanize(modifier),
+        attributes:
+          %{
+            rows: @rows
+          }
+          |> add_modifier(modifier),
+        slots: @slots
+      }
+    end)
+  end
+
+  def all_mods do
+    [
+      %Variation{
+        id: :all_mods,
+        description: "All modifiers",
+        attributes: %{
+          rows: @rows,
+          bordered: true,
+          striped: true,
+          narrow: true,
+          hoverable: true,
+          fullwidth: true
+        },
+        slots: @slots
+      }
+    ]
+  end
+
+  def add_modifier(attrs, modifier) do
+    Map.put(attrs, modifier, true)
+  end
 end
