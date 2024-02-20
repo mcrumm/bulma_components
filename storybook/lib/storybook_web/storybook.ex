94165b5efc0e4aa137ac16afd2326f324f813b21
Michael Crumm <michael.crumm@cargosense.com>
2024-02-20 11:31:12 -0800

    override heroicons in deps and tailwind config

--- storybook/lib/storybook_web/storybook.ex
+++ storybook/lib/storybook_web/storybook.ex
@@ -5,7 +5,7 @@ defmodule StorybookWeb.Storybook do
     # assets path are remote path, not local file-system paths
     css_path: "/assets/storybook.css",
     js_path: "/assets/storybook.js",
-    sandbox_class: "storybook-web",
+    sandbox_class: "bulma-components-sandbox",
     font_awesome_plan: :free,
     font_awesome_kit_id: "a86d7ff2ff",
     font_awesome_rendering: :webfont
