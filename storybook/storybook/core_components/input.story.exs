6d26bd13f18d9a07a83a879e8709654b579e2ab5
David Parry <david.parry@suranyami.com>
2024-02-23 09:29:24 +1100

    add more detailed input examples

--- storybook/storybook/core_components/input.story.exs
+++ storybook/storybook/core_components/input.story.exs
@@ -13,28 +13,77 @@ defmodule Storybook.CoreComponents.Input do
     """
   end
 
+  @attrs [
+    [
+      :text,
+      %{
+        color: "primary",
+        icon: "font",
+        icon_color: "success",
+        icon_size: :small,
+        icon_align: :left,
+        errors: ["This is an error message"]
+      }
+    ],
+    [
+      :text,
+      %{
+        color: "primary",
+        icon: "font",
+        icon_color: "success",
+        icon_size: :small,
+        icon_align: :left
+      }
+    ]
+
+    # [:password, %{icon: "lock"}],
+    # [:email, %{value: "fred@example.com", icon: "check", color: "link"}],
+    # [:textarea, %{icon: "envelope", color: "info", rows: 4}],
+    # [:number, %{}],
+    # [:date, %{icon: "calendar"}],
+    # [:color, %{icon: "paintbrush"}],
+    # [:checkbox, %{}],
+    # [
+    #   :select,
+    #   %{
+    #     icon: "globe",
+    #     options: ["Option 1", "Option 2", "Option 3"]
+    #   }
+    # ],
+    # [:file, %{icon: "upload"}],
+    # [:radio, %{icon: "check"}],
+    # [:range, %{icon: "sliders"}],
+    # [:search, %{icon: "search"}],
+    # [:tel, %{icon: "phone"}],
+    # [:url, %{icon: "link"}],
+    # [:datetime, %{icon: "calendar"}]
+  ]
+
+  def story_vars do
+    for [type, type_attrs] <- @attrs do
+      attrs =
+        %{
+          type: to_string(type),
+          label: String.capitalize("#{type}")
+        }
+        |> Map.merge(type_attrs)
+
+      id =
+        "#{type}_#{UUID.uuid4()}"
+        |> String.to_atom()
+
+      %Variation{
+        id: id,
+        attributes: attrs
+      }
+    end
+  end
+
   def variations do
     [
       %VariationGroup{
         id: :basic_inputs,
-        variations:
-          for type <- ~w(text email textarea number date color range checkbox)a do
-            %Variation{
-              id: type,
-              attributes: %{
-                type: to_string(type),
-                label: String.capitalize("#{type} input")
-              }
-            }
-          end
-      },
-      %Variation{
-        id: :select,
-        attributes: %{
-          label: "Select input",
-          type: "select",
-          options: ["Option 1", "Option 2", "Option 3"]
-        }
+        variations: story_vars()
       }
     ]
   end
